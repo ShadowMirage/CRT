af5bc0e27a2751c61edf69a6cabbf69e7697c963
ShadowMirage <prajapattanmay@gmail.com>
2026-05-12 09:38:28 +0530

    normal commit

--- Practice cpp/right_angle_triangle.cpp
+++ Practice cpp/right_angle_triangle.cpp
@@ -6,8 +6,10 @@ int main() {
   cout << "Enter a number:" << endl;
   cin >> n;
 
-  for (int i = 1; i <= 10; i++) {
-    cout << n << "X" << i << "=" << n * i << endl;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++)
+      cout << "*";
+    cout << endl;
   }
   return 0;
 }
